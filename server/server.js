9498a2fb90abb820d5bfb2236eb23a2b8a689665
amlali <37159337+amlali@users.noreply.github.com>
2018-09-24 04:13:17 +0200

    modified

--- server/server.js
+++ server/server.js
@@ -1,12 +1,17 @@
 const path = require('path');
+const http = require('http');
 const publicPath=path.join(__dirname,'../public');
 const express = require('express');
+const socketIO = require('socket.io');
 var port=process.env.PORT||3000;
 var app=express();
+const server=http.createServer(app);
+var io=socketIO(server);
 app.use(express.static(publicPath));
 
-
-
-app.listen(port,()=>{
+io.on('connection',()=>{
+  console.log("connected");
+});
+server.listen(port,()=>{
   console.log(`listen to port${port}`);
 });
